4de853ad01c3e81e4f8acf7cacaf877354c486dc
Arnab Datta <dattaarnab2013@gmail.com>
2026-02-04 23:55:22 +0530

    conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
+  cosnt[(from, setFrom)] = useState("usd");
+  cosnt[(to, setFTo)] = useState("inr");
+  cosnt[(convertedAmount, setConvertedAmount)] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 
   return (
     <>
